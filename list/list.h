911564102fedec96a028784bdfbcd39bf0224b38
zhengfh <zhengfh@sinoregal.cn>
2019-03-01 17:00:54 +0800

    重温单列表列表实现

---
+++ list/list.h
@@ -0,0 +1,62 @@
+#ifndef LIST_H__
+#define LIST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int nValue;
+	struct node *pNext;
+}node_t;
+
+int add(node_t **ppHead, int nValue)
+{
+	node_t *pHead = *ppHead;
+#if 0
+	//printf ("pphead:%p, phead:%p, *pphead:%p\n", ppHead, pHead, *ppHead);
+	if (*ppHead == NULL) {
+		(*ppHead) = (node_t*)malloc (sizeof (node_t));
+		(*ppHead)->nValue = nValue;
+		(*ppHead)->pNext = NULL;
+		//printf ("value 1:%d\n", nValue);
+	} else {
+		node_t **tmp = &((*ppHead)->pNext);
+		while (*tmp) {
+			tmp = &(*tmp)->pNext;
+		}
+		//printf ("value:%d\n", nValue);
+		(*tmp) = (node_t*)malloc (sizeof (node_t));
+		(*tmp)->nValue = nValue;
+		(*tmp)->pNext = NULL;
+	}
+#else 
+		node_t **tmp = ppHead;
+		while (*tmp) {
+			tmp = &(*tmp)->pNext;
+		}
+		//printf ("value:%d\n", nValue);
+		(*tmp) = (node_t*)malloc (sizeof (node_t));
+		(*tmp)->nValue = nValue;
+		(*tmp)->pNext = NULL;
+#endif //0
+	
+	return 0;
+}
+
+int print(node_t *pHead)
+{
+	node_t *tmp = pHead;
+	while (tmp) {
+		printf ("print value:%d\n", tmp->nValue);
+		tmp = tmp->pNext;
+	}
+	
+	return 0;
+}
+
+int del(node_t *pHead, int nValue)
+{
+	return 0;
+}
+
+#endif // LIST_H__
---
+++ list/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+	node_t *pHead = NULL;
+	int i = 0;
+	printf ("ready add\n");
+	for (i=0; i<10; i++) {
+		add (&pHead, i);
+	}
+	printf ("ready print\n");
+	print (pHead);
+
+	return 0;
+}
