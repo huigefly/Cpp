a2bb64923be0910174809a158515678511fbd4a8
zhengfuhui <zhengfuhui@ruijie.com.cn>
2020-09-01 16:09:22 +0800

    test freshman code question

--- test_mix_demo/debug.cpp
+++ test_mix_demo/debug.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
+#include <stdlib.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define DEBUG(x) cout<<#x<<"="<<x<<endl 
@@ -10,11 +12,29 @@ class Record{
 	FILED(TWO);
 	FILED(THREE);
 };
+
+/*
+
+*/
+string get()
+{
+	string sBuf = "";
+	char *pszBuf = new char[64];
+	sprintf (pszBuf, "hellworold____nishuosha");
+	printf("%s\n", pszBuf);
+	sBuf = pszBuf;
+	printf("%s\n", sBuf.c_str());
+	delete[] pszBuf;
+	return sBuf;
+}
+
 int main(){
 
 	int a=10;
 	DEBUG (a);
 
+	string sBuf = get();
+	printf("get buf:%s\n", sBuf.c_str());
 	//for (int i=1; i<100; i++)
 		TRACE(a);
 	return 0;
